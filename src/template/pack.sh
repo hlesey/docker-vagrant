024e824d34dde7ef7b3113e4cf3bb4ba1807aee9
Victor Varza <victor.varza@gmail.com>
2019-08-06 08:35:34 +0300

    update docker image

--- src/template/pack.sh
+++ src/template/pack.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 vagrant package --output output/docker-base.box 
-vagrant box add output/docker-base.box --name qedzone/docker-base
+vagrant box add output/docker-base.box --name hlesey/docker-base
